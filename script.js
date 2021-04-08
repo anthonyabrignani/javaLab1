2880c37ab1d934dae3920de5ed5546efbdabda8b
anthony abrignani <anthony abrignani>
2021-04-08 17:01:58 -0400

    all script before rng

---
+++ script.js
@@ -0,0 +1,22 @@
+const name = ("Anthony Abrignani");
+const age = 29;
+const birthday = ("March 30");
+const lifeEvents = ["I was born in Detroit, Michigan", "My first music festival was in 2011",
+                   "I graduated high school in 2010", "I adopted my cat the day after Thanksgiving 2019"]
+let detroitGC = true;
+
+if (detroitGC === true) {
+    console.log("My name is", name, "and I am a student at Grand Circus in Detroit, Michigan. I am currently", age, "years old and my birthday is on", birthday, ".");
+} else {
+    console.log("My name is", name, "and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently", age, "years old and my birthday is on", birthday, ".");
+}
+
+for (let i = 0; i < lifeEvents.length; i++) {
+    console.log(lifeEvents[i]);
+  }
+
+const counter = 0;
+
+while (counter < 11) {
+    
+}
